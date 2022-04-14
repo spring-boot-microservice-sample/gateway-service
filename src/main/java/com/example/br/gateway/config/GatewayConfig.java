5aa781e06ae4476a5e2399f5d19e932f5ba25a96
“rohithegde-sankey” <rohit.h@sankeysolutions.com>
2022-04-14 09:22:50 +0530

    initial commit

---
+++ src/main/java/com/example/br/gateway/config/GatewayConfig.java
@@ -0,0 +1,28 @@
+package com.example.br.gateway.config;
+
+import com.example.br.gateway.filter.AuthenticationFilter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GatewayConfig {
+
+//    @Autowired
+//    private AuthenticationFilter filter;
+
+    private String baseUrl = "http://localhost:";
+
+    @Bean
+    public RouteLocator routes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("event",
+                        r -> r.path("/event/**")
+//                                .filters(f -> f.filter(filter))
+                                .uri("lb://EVENTS-SERVICE")
+                )
+                .build();
+    }
+}
---
+++ src/main/java/com/example/br/gateway/filter/AuthenticationFilter.java
@@ -0,0 +1,65 @@
+package com.example.br.gateway.filter;
+
+import com.example.br.gateway.jwt.JwtUtil;
+import io.jsonwebtoken.Claims;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+import java.util.function.Predicate;
+
+//@Component
+public class AuthenticationFilter implements GatewayFilter {
+
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+
+        ServerHttpRequest request = (ServerHttpRequest) exchange.getRequest();
+
+        final List<String> apiEndpoints = List.of("/register", "/login");
+        Predicate<ServerHttpRequest> isApiSecured = (req) -> apiEndpoints.stream()
+                .noneMatch(uri -> req.getURI().getPath().contains(uri));
+
+        if(isApiSecured.test(request)){
+
+//            Check Header
+            if(!request.getHeaders().containsKey("Authorization")){
+
+                ServerHttpResponse response = exchange.getResponse();
+                response.setStatusCode(HttpStatus.UNAUTHORIZED);
+
+                return response.setComplete();
+            }
+
+            final String token = request.getHeaders().getOrEmpty("Authorization").get(0);
+
+            try {
+                jwtUtil.validateToken(token);
+            } catch (Exception e) {
+
+                ServerHttpResponse response = exchange.getResponse();
+                response.setStatusCode(HttpStatus.BAD_REQUEST);
+
+                return response.setComplete();
+            }
+
+            Claims claims = jwtUtil.getClaims(token);
+            exchange.getRequest().mutate().header(
+                    "id",
+                    String.valueOf(claims.get("id"))
+            ).build();
+        }
+
+        return  chain.filter(exchange);
+    }
+}
